d76f2fac61024a57b0f4624b312108def0bd5789
cody <codyjeromegreen@gmail.com>
2017-10-28 11:22:32 -0700

    more or less done on this one so far

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
-var ul_lists = document.getElementsByTagName("ul");
+var ul_lists = document.getElementsByTagName('ul');
 
 function update_list(list) {
-  list.style.listStyle = "none";
-  list.style.webkitPaddingStart = "10px";
-  list.style.MozPaddingStart = "10px";
+  list.style.listStyle = 'none';
+  list.style.webkitPaddingStart = '10px';
+  list.style.MozPaddingStart = '10px';
 }
 
-for (var idx = 0; idx < ul_lists.length; idx++) {
-  update_list(ul_lists[idx]);
+for (var i = 0; i < ul_lists.length; i++) {
+  update_list(ul_lists[i]);
 }
